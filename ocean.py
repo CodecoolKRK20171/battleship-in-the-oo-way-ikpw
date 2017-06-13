07ba49cf9806b4eae9120e499dfa544f660d4c06
Ragnarockie08 <Ragnarockie08@gmail.com>
2017-06-13 15:58:56 +0200

    added function format board that prints board

--- ocean.py
+++ ocean.py
@@ -1,7 +1,31 @@
+from ships import Ship
+from squares import Square
+
+
 class Ocean:
 
     def __init__(self):
-        pass
+        self.ships = []
+        self.board = []
 
     def __str__(self):
-        pass
+
+        ocean = ''
+        for line in self.board:
+            ocean += '|'
+            line.insert(-1, '|')
+            for square in line:
+                ocean += str(square)
+
+        return ocean
+
+
+    def format_board(self):
+
+        self.board = []
+        for i in range(0, 10):
+            squares_list = []
+            for j in range(0, 10):
+                squares_list.append(Square(i, j))
+            squares_list.append("\n")
+            self.board.append(temp_list)
