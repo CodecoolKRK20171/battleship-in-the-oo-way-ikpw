709f06fa1f2698e2cb458f08b0edf37da6e16eea
Ragnarockie08 <Ragnarockie08@gmail.com>
2017-06-14 17:33:39 +0200

    new method that change squares status added

--- squares.py
+++ squares.py
@@ -4,22 +4,23 @@ class Square:
 
         self.row = row
         self.column = column
-        self.is_empty = True
+        self.is_water = True
         self.is_ship = False
+
+    def fill_square(self):
+        self.is_water = False
+
+    def set_as_ship(self):
+        self.is_ship = True
 
     def __str__(self):
 
-        if self.is_empty:
-            mark = ' '
+        if self.is_water:
+            mark = '~'
         else:
             if self.is_ship:
                 mark = 'X'
             else:
                 mark = '0'
+
         return mark
-
-    def fill_square(self):
-        self.is_empty = False
-
-    def ship(self):
-        self.is_ship = True
