609f352ae2053a82856f0d19c47eec1794fec969
Ragnarockie08 <Ragnarockie08@gmail.com>
2017-06-14 09:13:23 +0200

    add ship method functionality changed

--- player.py
+++ player.py
@@ -1,19 +1,39 @@
+from ships import Ship
+from squares import Square
 
 class Player:
 
-    def __init__(self, name):
-        self.name = name
-        self.is_living = True
+    ships = {"Carrier": 5,
+            "Battleship": 4,
+            "Cruiser": 3,
+            "Submarine": 3,
+            "Destroyer": 2}
 
-    def attack_position(self, board, x, y):
+    def __init__(self, ship_name, ocean):
+
+        self.ocean = ocean
+        self.size = Ship.ships[ship_name]
 
 
-    def place_ships_on_board(self, input):
+    def attack_position(self, positions):
+        self.ocean.board[positions[0]][positions[1]].fill_square()
+
+
+    def add_ship(self, positions, size, is_vertical):
+
+        for i in range(self.size):
+            if is_vertical:
+                self.ocean.board[positions[1] + i][positions[0]].ship()
+            else:
+                self.ocean.board[positions[1]][positions[0] + i].ship()
+
+        return positions
 
 
 
     def valid_position(self, name, coordinates):
-
+        pass
 
 
     def win_lose(self):
+        pass
