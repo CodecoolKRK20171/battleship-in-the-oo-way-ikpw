c1e7e7223e94a207b13e75b958d21be9eae89834
Ragnarockie08 <Ragnarockie08@gmail.com>
2017-06-14 17:35:19 +0200

    letters printed next to board deleted because of problems with indexes

--- ocean.py
+++ ocean.py
@@ -5,29 +5,37 @@ from squares import Square
 class Ocean:
 
     def __init__(self):
-        self.ships = []
         self.board = []
 
     def __str__(self):
 
-        letters = 'ABCDEFGHIJ'
-        index = 0
         ocean = ''
+        ocean += '0123456789\n'
         for line in self.board:
-            ocean += '{}'.format(index) +' |'
-            line.insert(-1, '|')
-            index += 1
             for square in line:
                 ocean += str(square)
 
         return ocean
 
+    def attack_position(self, positions):
+
+        self.ocean.board[positions[0]][positions[1]].fill_square()
 
     def format_board(self):
 
+        index = 0
+        letters = 'ABCDEFGHIJ'
         self.board = []
         for i in range(0, 10):
             self.board.append([])
             for j in range(0, 10):
                 self.board[i].append(Square(i, j))
             self.board[i].append("\n")
+
+        self.second_board = self.board[:]
+
+        #for line in self.second_board:
+            #line.insert(0, letters[index])
+            #line.insert(1, '|')
+            #line.append('|')
+            #index += 1
