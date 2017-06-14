afee30cc79b54943d96740b712b34654d901127b
Ragnarockie08 <Ragnarockie08@gmail.com>
2017-06-14 17:37:25 +0200

    small letter changes

---
+++ ships.py
@@ -0,0 +1,16 @@
+from squares import Square
+
+
+class Ship:
+
+    ships = {"Carrier": 5,
+            "Battleship": 4,
+            "Cruiser": 3,
+            "Submarine": 3,
+            "Destroyer": 2}
+
+    def __init__(self, positions, name, is_vertical):
+
+        self.name = name
+        self.positions = positions
+        self.is_vertical = is_vertical
