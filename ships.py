b8d48b9b6d58d9f9ab9cc857e95bc2738c946379
polbox93 <polbox93@gmail.com>
2017-06-14 10:22:52 +0200

    ships module added

---
+++ ships.py
@@ -0,0 +1,15 @@
+from squares import Square
+
+
+class Ship:
+
+   ships = {"Carrier": 5,
+           "Battleship": 4,
+           "Cruiser": 3,
+           "Submarine": 3,
+           "Destroyer": 2}
+
+   def __init__(self):
+       self.name = name
+       self.positions = positions
+       self.is_vertical = is_vertical
