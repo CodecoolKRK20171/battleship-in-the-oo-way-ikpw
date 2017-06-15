077bdb83589ea0555bda1e1f99808f4bfc07b580
Ragnarockie08 <Ragnarockie08@gmail.com>
2017-06-15 15:42:09 +0200

    small letters fixed

--- squares.py
+++ squares.py
@@ -4,22 +4,29 @@ class Square:
 
         self.row = row
         self.column = column
-        self.is_water = True
+        self.is_empty = True
         self.is_ship = False
+        self.is_water = False
 
     def fill_square(self):
-        self.is_water = False
+        self.is_empty = False
 
     def set_as_ship(self):
         self.is_ship = True
 
+    def set_water(self):
+        self.is_water = True
+
+
     def __str__(self):
 
-        if self.is_water:
+        if self.is_empty:
             mark = '~'
         else:
             if self.is_ship:
                 mark = 'X'
+            elif self.is_water:
+                mark = ' '
             else:
                 mark = '0'
 
